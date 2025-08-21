8e0a6e7edca06665b08abf9c1f2833b442426889
Varnika Jain <varnikajain.it24@gmail.com>
2025-08-21 11:19:43 +0530

    Fetch details in detail page

--- app/src/main/java/com/varnika_jain/pokedex/ui/details/DetailFragment.kt
+++ app/src/main/java/com/varnika_jain/pokedex/ui/details/DetailFragment.kt
@@ -1,5 +1,6 @@
 package com.varnika_jain.pokedex.ui.details
 
+import android.content.res.ColorStateList
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.util.Log
@@ -7,8 +8,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 import androidx.core.graphics.ColorUtils
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.palette.graphics.Palette
 import com.google.android.material.textview.MaterialTextView
@@ -22,6 +25,7 @@ import com.varnika_jain.pokedex.utils.buildImageUrl
 import com.varnika_jain.pokedex.utils.collectFlow
 import com.varnika_jain.pokedex.utils.loadImage
 import com.varnika_jain.pokedex.utils.viewModelFactory
+import java.util.ArrayList
 
 
 class DetailFragment : Fragment() {
@@ -30,18 +34,24 @@ class DetailFragment : Fragment() {
     }
     private lateinit var binding: FragmentDetailsBinding
     private lateinit var pokemonDetails: PokemonDetails
+    private lateinit var adapter: PokeDetailsAdapter
     private val args: DetailFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmentDetailsBinding.inflate(layoutInflater)
+
+        binding.backBtn.setOnClickListener { findNavController().navigateUp() }
+        adapter = PokeDetailsAdapter(
+            requireContext(), arrayListOf()
+        )
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val inflater = LayoutInflater.from(requireContext())
 
         viewModel.fetchPokemonDetails(args.pokemonId)
 
@@ -53,41 +63,8 @@ class DetailFragment : Fragment() {
 
                 is Result.Success -> {
                     pokemonDetails = result.data
-                    binding.ivPokemonImg.loadImage(
-                        imageUrl = args.pokemonId.buildImageUrl(),
-                        allowCaching = true,
-                        imageLoadListener = { state ->
-                            when (state) {
-                                is ImageLoadState.Loading -> {
-                                    Log.d("TAG", "onViewCreated: Image is loading....")
-                                }
-
-                                is ImageLoadState.Success -> {
-
-                                    val drawable = binding.ivPokemonImg.drawable
-                                    if (drawable is BitmapDrawable) {
-                                        Palette.from(drawable.bitmap).generate { palette ->
-                                            val swatch =
-                                                palette?.dominantSwatch ?: palette?.vibrantSwatch
-                                            swatch?.let {
-                                                binding.ivPokemonImg.setBackgroundColor(
-                                                    ColorUtils.setAlphaComponent(
-                                                        it.rgb, (0.7f * 255).toInt()
-                                                    )
-                                                )
-                                            }
-                                        }
-                                    }
-
-                                }
-
-                                is ImageLoadState.Error -> {
-                                    Log.e(
-                                        "ImageView.loadImage", "Image load failed", state.throwable
-                                    )
-                                }
-                            }
-                        })
+                    setupPokemonImage()
+                    setupPokemonDetails()
                     Log.d("TAG", "onViewCreated: Success... Result ${result.data.name} ")
                 }
 
@@ -97,15 +74,89 @@ class DetailFragment : Fragment() {
             }
         }
 
-        val parentLayout = view.findViewById<LinearLayout>(R.id.layoutPowerTypes)
-        val powerTypes = listOf("Water", "Electric")
+    }
 
-        parentLayout.removeAllViews()
-        for (type in powerTypes) {
+    private fun setupPokemonImage() {
+        binding.ivPokemonImg.loadImage(
+            imageUrl = args.pokemonId.buildImageUrl(),
+            allowCaching = true,
+            imageLoadListener = { state ->
+                when (state) {
+                    is ImageLoadState.Loading -> {
+                        Log.d("TAG", "onViewCreated: Image is loading....")
+                    }
+
+                    is ImageLoadState.Success -> {
+
+                        val drawable = binding.ivPokemonImg.drawable
+                        if (drawable is BitmapDrawable) {
+                            Palette.from(drawable.bitmap).generate { palette ->
+                                val swatch =
+                                    palette?.dominantSwatch ?: palette?.vibrantSwatch
+                                swatch?.let {
+                                    val bgColor =
+                                        ColorUtils.setAlphaComponent(it.rgb, (0.7f * 255).toInt())
+                                    binding.ivPokemonImg.setBackgroundColor(bgColor)
+                                    tintPowerTypes(bgColor)
+                                }
+                            }
+                        }
+
+                    }
+
+                    is ImageLoadState.Error -> {
+                        Log.e(
+                            "ImageView.loadImage", "Image load failed", state.throwable
+                        )
+                    }
+                }
+            })
+    }
+
+    private fun setupPokemonDetails() {
+        binding.tvNamePokemon.text =
+            pokemonDetails.name?.replaceFirstChar { c -> c.uppercaseChar() }
+        binding.tvWeightValue.text =
+            String.format(getString(R.string.str_pokemon_weight), pokemonDetails.weight)
+        binding.tvHeightValue.text =
+            String.format(getString(R.string.str_pokemon_height), pokemonDetails.height)
+        setPokemonTypes()
+        setPokemonStats()
+    }
+
+    private fun setPokemonStats() {
+        val pokemonStats = pokemonDetails.stats
+        val statList = ArrayList<PokemonDetails.Stats>(pokemonStats)
+        binding.rvStats.adapter = adapter
+        if (!pokemonStats.isNullOrEmpty()){
+            adapter.submitStatsList(statList)
+        }
+
+    }
+
+    private fun setPokemonTypes() {
+        val inflater = LayoutInflater.from(requireContext())
+        val parentLayout = view?.findViewById<LinearLayout>(R.id.layoutPowerTypes)
+        val pokemonTypes = pokemonDetails.types
+
+        parentLayout?.removeAllViews()
+        pokemonTypes?.forEach { types ->
             val textView =
                 inflater.inflate(R.layout.item_power_type, parentLayout, false) as MaterialTextView
-            textView.text = type
-            parentLayout.addView(textView)
+            textView.text = types?.type?.name
+            textView.backgroundTintList =
+                ContextCompat.getColorStateList(requireContext(), R.color.primary)
+            parentLayout?.addView(textView)
+        }
+    }
+
+    private fun tintPowerTypes(color: Int) {
+        val parentLayout = binding.layoutPowerTypes
+        for (i in 0 until parentLayout.childCount) {
+            val child = parentLayout.getChildAt(i)
+            if (child is MaterialTextView) {
+                child.backgroundTintList = ColorStateList.valueOf(color)
+            }
         }
     }
 }
--- app/src/main/java/com/varnika_jain/pokedex/ui/details/PokeDetailsAdapter.kt
+++ app/src/main/java/com/varnika_jain/pokedex/ui/details/PokeDetailsAdapter.kt
@@ -1,4 +1,49 @@
 package com.varnika_jain.pokedex.ui.details
 
-class PokeDetailsAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.varnika_jain.pokedex.data.remote.PokemonDetails
+import com.varnika_jain.pokedex.databinding.ItemStatsBarBinding
+
+class PokeDetailsAdapter(
+    private val context: Context,
+    private var statsList: ArrayList<PokemonDetails.Stats>,
+) : RecyclerView.Adapter<PokeDetailsAdapter.DetailViewHolder>() {
+    class DetailViewHolder(val binding: ItemStatsBarBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): DetailViewHolder {
+        return DetailViewHolder(
+            ItemStatsBarBinding.inflate(
+                LayoutInflater.from(context), parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(
+        holder: DetailViewHolder,
+        position: Int
+    ) {
+        with(holder) {
+            binding.apply {
+                tvStatLabel.text = statsList[position].stat?.name
+                statsList[position].baseStat?.let { progressStats.setProgress(it, true) }
+                tvStatValue.text = statsList[position].baseStat.toString()
+            }
+        }
+    }
+
+    override fun getItemCount() = statsList.size
+
+    fun submitStatsList(list: ArrayList<PokemonDetails.Stats>) {
+        statsList.clear()
+        statsList.addAll(list)
+        notifyDataSetChanged()
+    }
+
 }
