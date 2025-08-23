3e22a5f1dd5a6d5cd91d4b69138e5ffcd57f93ca
Harshkumar.Patel <Harshkumar.Patel@valtech.com>
2025-08-23 08:11:39 +0530

    refactor: provide fullscreen support to app.

--- app/src/main/java/com/varnika_jain/pokedex/ui/MainActivity.kt
+++ app/src/main/java/com/varnika_jain/pokedex/ui/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.varnika_jain.pokedex.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.varnika_jain.pokedex.databinding.ActivityMainBinding
@@ -14,8 +16,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.hide(WindowInsetsCompat.Type.statusBars() or WindowInsetsCompat.Type.navigationBars())
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
 
-        enableEdgeToEdge()
         setContentView(binding.root)
         val navHostFragment =
             supportFragmentManager.findFragmentById(binding.fragmentContainer.id) as NavHostFragment
